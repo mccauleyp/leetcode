1a52fbf7eec8f70b96a44d6a41adb51b19b1823e
pmcc8541 <pmcc8541@uni.sydney.edu.au>
2020-08-23 01:55:47 -0400

    added difficulty directories

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,4 +1,7 @@
 '''
+https://leetcode.com/problems/two-sum/
+1. Two Sum
+Easy
 https://leetcode.com/problems/two-sum
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
